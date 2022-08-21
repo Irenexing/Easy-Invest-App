932292361bb2f14b0875669ee6db07cde8c29595
Irene Xing <irenexing0220@gmail.com>
2022-08-21 02:06:43 -0400

    text

--- easy-invest-app/src/components/Module1Page/Module1Page1.js
+++ easy-invest-app/src/components/Module1Page/Module1Page1.js
@@ -18,26 +18,29 @@ return (
       allowFullScreen
       title="Embedded youtube"
     />
-    <div>CHAPTER 1</div>
+    <div className="chapter">CHAPTER 1</div>
     <h1>The benefit of investing early</h1>
+    <br></br>
+    <br></br>
     <p>Investing when you’re young is one of the best ways to see solid returns on your money.</p>
     <br></br>
-    <p>a little goes a long way...</p>
+    <br></br>
+    <p className="bold-text">A little goes a long way...</p>
     <ul>
-        <li>Compound earnings: your investment returns start earning their own return.</li>
-        <li>Compounding allows your account balance to snowball over time.</li> 
+        <li><b>Compound earnings:</b> your investment returns start earning their own return.</li>
+        <li><b>Compounding</b> allows your account balance to snowball over time.</li> 
     </ul>
     <br></br>
-    <p>Let's take an example...</p>
+    <p className="bold-text">Let's take an example...</p>
     <br></br>
     <img className="module__image" src="images/Frame 40.png"/>
     <br></br>
-    <p>Ride out the waves...</p>
+    <p className="bold-text">Ride out the waves...</p>
     <ul>
         <li>Investing young means you have decades to wait out the ups and downs of the stock market</li>
     </ul>
     <br></br>
-    <p>Let's take the royal Bank of Canada as an example</p>
+    <p className="bold-text">Let's take the royal Bank of Canada as an example</p>
     <br></br>
     <p>Compare the graph results for 5 years. What do you notice?</p>
     <img className="module__image" src="images/graph.png"/>
--- easy-invest-app/src/components/Module1Page/Module1Page3.js
+++ easy-invest-app/src/components/Module1Page/Module1Page3.js
@@ -19,15 +19,15 @@ return (
       allowFullScreen
       title="Embedded youtube"
     />
-    <div>CHAPTER 3</div>
+    <div className="chapter">CHAPTER 3</div>
     <h1>INVESTMENT TYPES</h1>
     <ol className="list">
-        <li className="list-items">High-Interest Savings Account</li>
+        <li className="list-items bold-text">High-Interest Savings Account</li>
             <ul>
               <li>Safest way - no volatility</li>
               <li>Earning guaranteed annual interest around 1.3%</li>
             </ul>
-        <li className="list-items">Money Market Account</li>
+        <li className="list-items bold-text">Money Market Account</li>
             <ul>
               <li>Safest way - no volatility</li>
               <li>Another type of high-interest savings account</li>
@@ -35,14 +35,14 @@ return (
               <li>Returning around 1%-2% interest</li>
               <li>Potential monthly fees</li>
             </ul>
-        <li className="list-items">Certificate of Deposit</li>
+        <li className="list-items bold-text">Certificate of Deposit</li>
             <ul>
             <li>Safest way - no volatility </li>
             <li>Promissory note from bank </li>
             <li>Fixed interest over a specified amount of time</li>
             <li>However, during that time it cannot be converted to cash without a penalty. </li>
             </ul>
-        <li className="list-items">Bond</li>
+        <li className="list-items bold-text">Bond</li>
             <ul>
               <li>Bit riskier, corporate bonds aren't guaranteed by the full faith and credit</li>
               <li>A Note issued by the government or corporations when they're looking to fund a new project.</li>
@@ -50,26 +50,26 @@ return (
               <li>As interest rates fall, bond prices rise. Vice Versa </li>
               <li>During that time it cannot be converted to cash </li>
             </ul>
-        <li className="list-items">Mutual Fund</li>
+        <li className="list-items bold-text">Mutual Fund</li>
           <ul>
             <li>Pre-diversified collections of investments, safer than individual stocks</li>
             <li>Vary in risk depending on the type of fund</li>
             <li>Operated by a fund manager that chooses and maintains the portfolio</li>
           </ul>
-        <li className="list-items">Exchange Traded Fund (ETF)</li>
+        <li className="list-items bold-text">Exchange Traded Fund (ETF)</li>
           <ul>
             <li>Similar to mutual funds but bit riskier</li>
             <li>and are traded on an exchange like stocks</li>
             <li>More tax advantages compared to mutual funds</li>
             <li>ETFs are a more hands-on investment than mutual funds</li>
           </ul>
-        <li className="list-items">Stock</li>
+        <li className="list-items bold-text">Stock</li>
           <ul>
             <li>Riskier than ETFs because there is no diversification</li>
             <li>It is a very hands-on investment </li>
             <li>A stock is a share of ownership of a company</li>
           </ul>
-        <li className="list-items">Real Estate</li>
+        <li className="list-items bold-text">Real Estate</li>
           <ul>
             <li>Requires a lot of capital</li>
             <li>Could flip properties or rent out properties</li>
